73da4b00708ca273623886d55a47ca1ad5188c02
masood <masoodacheampong@gmail.com>
2024-10-08 11:58:07 +0000

    chore:implementing landing page ui using prismic and nextjs

---
+++ src/app/fonts/fonts.ts
@@ -0,0 +1,7 @@
+import localFont from "next/font/local";
+
+export const getMonaSans = localFont({
+  src: "./Mona-Sans.woff",
+  variable: "--font-mona-sans",
+  weight: "100 900",
+});
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,8 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      fontFamily: {
+        body: ["var(--font-mona-sans)", "sans-serif"],
       },
     },
   },
